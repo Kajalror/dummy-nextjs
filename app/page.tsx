22e79a06b14ecc2ccf26a6404056f335fcd8bf1c
Kajalror <krulyan90@gmail.com>
2024-05-23 15:32:22 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1> hello home</h1>
       <h1> Welcome to my page </h1>
+      <p> hey whats up </p>
     </main>
   );
 }
